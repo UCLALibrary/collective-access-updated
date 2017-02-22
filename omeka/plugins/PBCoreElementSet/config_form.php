879d7e4f831df2acff30934da1ba700ef47d81f2
alegaeta <alegaeta3@gmail.com>
2017-02-22 14:20:59 -0800

    collective access

---
+++ omeka/plugins/PBCoreElementSet/config_form.php
@@ -0,0 +1,11 @@
+<div class="field">
+    <div id="PBCoreElementSetAddUrlAsIdentifier_label" class="one columns alpha">
+        <?php echo get_view()->formLabel('PBCoreElementSetAddUrlAsIdentifier', __('Add url as identifier'));?>
+    </div>
+    <div class="inputs">
+        <?php echo get_view()->formCheckbox('PBCoreElementSetAddUrlAsIdentifier', true, array('checked' => (boolean) get_option('pbcore_element_set_add_url_as_identifier')));?>
+        <p class="explanation"><?php echo __(
+            'This option adds automatically the url of the item ("http://myomeka.com/items/show/#") to the metadata "PBCore > Identifier".'
+        );?></p>
+    </div>
+</div>
